555fffe07a169617a2d8071269ec8dfcc2142e2d
Weltear <weltear0219@gmail.com>
2025-03-31 20:24:39 +0800

    第1题 两数之和

--- src/1.两数之和.cpp
+++ src/1.两数之和.cpp
@@ -5,10 +5,24 @@
  */
 
 // @lc code=start
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> map;
+        vector<int> res;
         
+        for (int i = 0; i < nums.size(); ++i) {
+            int diff = target - nums[i];
+            auto another = map.find(diff);
+            if (another != map.end()) return vector<int> {another->second, i};
+            map[nums[i]] = i;
+        }
+        return {};
     }
 };
 // @lc code=end
